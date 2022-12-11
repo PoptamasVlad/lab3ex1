d6682ac013691d8b2450984d865261fb5a576904
Poptamas Vlad <poptamasvlad@gmail.com>
2022-12-11 12:50:08 +0200

    Add project files.

---
+++ lab3ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab3ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti o functie care va calcula suma cifrelor unui numar. */
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SumaCifrelor(n));
+
+            static int SumaCifrelor(int n)
+            {
+                int suma = 0;
+                while(n>0)
+                {
+                    suma = suma + n%10;
+                    n = n / 10;
+                }
+                return suma;
+            }
+        }
+    }
+}
